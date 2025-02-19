fcba0beb8a137432c534b84d83d4c00386875ff0
junior martinez <martinezjuniormanuel175@gmail.com>
2025-02-19 12:48:51 -0600

    save

--- weather-app/jest.setup.js
+++ weather-app/jest.setup.js
@@ -1,3 +1,3 @@
 import '@testing-library/jest-dom';
-process.env.NEXT_PUBLIC_WEATHER_API_KEY = "fake_api_key";
+process.env.NEXT_PUBLIC_WEATHER_API_KEY = "13193be8967bb46f0f4ca4feb9cdb1df";
 process.env.NEXT_PUBLIC_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather";
