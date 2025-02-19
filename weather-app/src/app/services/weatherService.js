ea0b3f800014c8985733cc236763fb3f38d53d2a
junior martinez <martinezjuniormanuel175@gmail.com>
2025-02-19 09:33:16 -0600

    save fix

---
+++ weather-app/src/app/services/weatherService.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+const BASE_URL = process.env.NEXT_PUBLIC_WEATHER_API_URL;
+export const fetchWeather = async (city) => {
+  if (!city) throw new Error("You must enter a city.");
+  if (!API_KEY || !BASE_URL) {
+    throw new Error("API configuration missing. Check environment variables.");
+  }
+  const formattedCity = city.trim();
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        q: formattedCity,
+        appid: API_KEY,
+        units: "metric",
+      },
+    });
+    return { success: true, data: response.data };
+  } catch (error) {
+    if (error.response) {
+      const { cod, message } = error.response.data;
+      
+      if (parseInt(cod, 10) === 404) {
+        return { success: false, message: "City not found. Please try again." };
+      } else {
+        return { success: false, message: `Error ${cod}: ${message}` };
+      }
+    } else if (error.request) {
+      return { success: false, message: "Connection error. Check your internet." };
+    } else {
+      return { success: false, message: "An unexpected error occurred. Please try again later." };
+    }
+  }
+};
